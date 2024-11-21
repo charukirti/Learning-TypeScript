84dcab13287d488c6d6e9fc19d8bf52f88ce04b8
charukirti <charukirticc112002@gmail.com>
2024-11-21 19:47:36 +0530

    installed and configured typescript project

---
+++ src/01-Types/01_string.ts
@@ -0,0 +1,8 @@
+let userName:string = "Charukirti";
+console.log(userName);
+
+let greet:string = "Good Evening!,";
+console.log(greet + userName);
+
+const age:string = '12';
+console.log('type of age is ' + typeof age, age);
